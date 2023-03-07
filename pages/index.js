243f37127ab0178357ab48780bf4a799bd348008
Niko Kreissl <nikokreissl@gmail.com>
2023-03-07 15:11:12 +0100

    Fetch API

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,11 @@
+import useSWR from "swr";
+
+const fetcher = (...args) => fetch(...args).then((res) => res.json());
+const URL = "https://example-apis.vercel.app/api/art";
+
 export default function HomePage() {
+  const { data } = useSWR(URL, fetcher);
+
   return (
     <div>
       <h1>Hello from Next.js</h1>
