d63edcb5b7799b0c6d9c416d3cb1c7add731e8a8
Niko Kreissl <nikokreissl@gmail.com>
2023-03-07 15:46:41 +0100

    Render art piece list

---
+++ components/ArtPieces/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import ArtPiecePreview from "../ArtPiecePreview";
+
+export default function ArtPieces({ pieces }) {
+  return (
+    <>
+      <h1>Pieces of Art</h1>
+      <ul>
+        {pieces.map((piece) => (
+          <li key={piece.slug}>
+            <ArtPiecePreview
+              image={piece.imageSource}
+              title={piece.name}
+              artist={piece.artist}
+            />
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,15 @@
 import useSWR from "swr";
+import ArtPieces from "../components/ArtPieces";
 
 const fetcher = (...args) => fetch(...args).then((res) => res.json());
 const URL = "https://example-apis.vercel.app/api/art";
 
 export default function HomePage() {
-  const { data } = useSWR(URL, fetcher);
+  const { data: pieces } = useSWR(URL, fetcher);
 
   return (
     <div>
-      <h1>Hello from Next.js</h1>
+      <ArtPieces pieces={pieces} />
     </div>
   );
 }
