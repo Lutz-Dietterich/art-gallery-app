6fc0043c7a98ae43a1f92299f009870eddea43ab
Niko Kreissl <nikokreissl@gmail.com>
2023-03-08 11:53:17 +0100

    Move fetch to _app and refactor spotlight

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,9 @@
-import useSWR from "swr";
-import ArtPieces from "../components/ArtPieces";
 import Spotlight from "../components/Spotlight";
 
-const fetcher = (...args) => fetch(...args).then((res) => res.json());
-const URL = "https://example-apis.vercel.app/api/art";
-
-export default function HomePage() {
-  const { data: pieces } = useSWR(URL, fetcher);
-
+export default function SpotlightPage() {
   return (
     <div>
-      <Spotlight pieces={pieces} />
-      <ArtPieces pieces={pieces} />
+      <Spotlight />
     </div>
   );
 }
