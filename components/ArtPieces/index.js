6955bd875e91384089f1f9b67fb831abfe5a7061
Lutz Dietterich <lutzdietterich@googlemail.com>
2023-03-08 10:03:44 +0100

    fix Loading issue

--- components/ArtPieces/index.js
+++ components/ArtPieces/index.js
@@ -2,6 +2,9 @@ import React from "react";
 import ArtPiecePreview from "../ArtPiecePreview";
 
 export default function ArtPieces({ pieces }) {
+  if (!pieces) {
+    return <p>Loading...</p>;
+  }
   return (
     <>
       <h1>Pieces of Art</h1>
