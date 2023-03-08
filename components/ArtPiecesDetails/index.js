f82e26a2ede9d55d119a12451a0c8735504193e0
Lutz Dietterich <lutzdietterich@googlemail.com>
2023-03-08 14:57:14 +0100

    add slug file

--- components/ArtPiecesDetails/index.js
+++ components/ArtPiecesDetails/index.js
@@ -1,3 +1,19 @@
-export default function ArtPiecesDetails() {
-  return <div>index</div>;
+import Image from "next/image";
+
+export default function ArtPiecesDetails({
+  image,
+  title,
+  artist,
+  year,
+  genre,
+}) {
+  return (
+    <main>
+      <h1>{title}</h1>
+      <Image src={image} alt={title} />
+      <h2>{artist}</h2>
+      <p>{year}</p>
+      <p>{genre}</p>
+    </main>
+  );
 }
